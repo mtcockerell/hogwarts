36b78ad869fe9cf4899ad87deb3836da86823859
mtcockerell <talmage123@gmail.com>
2022-04-06 10:51:02 -0500

    Initialcommit

---
+++ src/main/java/edu/tcu/cs/hogwartstartifactsonline/HogwartsartifactsonlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartstartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsartifactsonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsartifactsonlineApplication.class, args);
+	}
+
+}
